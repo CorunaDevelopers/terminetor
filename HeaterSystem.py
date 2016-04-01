587b3580629e0afd0456d3c80c9f0d1279df7fa9
victor <vtcmer@gmail.com>
2016-04-01 20:39:53 +0200

    creado clase base para el sistema de calefacción

---
+++ HeaterSystem.py
@@ -0,0 +1,10 @@
+class HeaterSystem():
+
+	def __init__(self,config):
+		self.config = config
+
+	def start():
+		raise "Method start() not implemented"
+	
+	def stop():
+		raise "Method stop() not implemented"
